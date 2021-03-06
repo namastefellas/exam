22ac590c354e12ad6de357d01550469ca086cae3
German <coolmatoff@gmail.com>
2021-03-06 16:47:41 +0600

    created page for adding new guests

--- exam/exam/urls.py
+++ exam/exam/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view
+from webapp.views import index_view, add_new_guest
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view)
+    path('', index_view, name='index'),
+    path('guest/add', add_new_guest, name='add_guest')
 ]
--- exam/webapp/views.py
+++ exam/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from webapp.models import GuestBook
 
 # Create your views here.
@@ -7,3 +7,19 @@ from webapp.models import GuestBook
 def index_view(request):
     guests = GuestBook.objects.all().filter(status='active').order_by('-date_start')
     return render(request, 'index.html', context={'guests': guests})
+
+def add_new_guest(request, *args, **kwargs):
+    if request.method == 'GET':
+        return render(request, 'add_guest.html')
+    if request.method == 'POST':
+        user_name = request.POST.get('user_name')
+        user_email = request.POST.get('user_email')
+        user_text = request.POST.get('user_text')
+
+    GuestBook.objects.create(
+        user_name=user_name,
+        user_email=user_email,
+        user_text=user_text
+    )
+
+    return redirect('index')
